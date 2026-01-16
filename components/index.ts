87facf86fe1c9557347fe7599ea51d785a9cedbf
jefebrydon <jefebrydon@users.noreply.github.com>
2026-01-16 15:19:00 -0800

    Update breakpoints: keep sparkle button visible until 480px, hide jeffbot-menu-item above 480px

--- components/index.ts
+++
@@ -1,5 +0,0 @@
-export { Button_Icon_Primary } from './Button_Icon_Primary';
-export { Button_Icon_Secondary } from './Button_Icon_Secondary';
-export { Message_User } from './Message_User';
-export { Message_JeffBot } from './Message_JeffBot';
-
